a6fcc5e5b027f0390f3e3dcd24a9e740ac5d0e8d
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-06-12 20:12:30 -0500

    add upperCaseArray strings

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var characterlength = 8;
 var choiceArray = [];
 
 var specialCharacterArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '|', '?', '/', '<', '>'];
-var lowerCaseArray = [];
-var upperCaseArray = [];
+var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
 var numberArray = [];
 
 // Get references to the #generate element
